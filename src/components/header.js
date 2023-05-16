d9e5a6c66608d5df320be56bec33da9709b59b66
Ranjeet <ranjeet0001@gmail.com>
2023-05-16 10:03:50 +0530

    k

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+
 class Header extends Component {
   render() {
     return (
       <>
-        <div>
-          <div>Logo</div>
+        <header>
+          <div className="logo">Logo</div>
           <input />
-        </div>
+        </header>
       </>
     );
   }
